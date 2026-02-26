d13c2a7bcf2addb17c643812897e57800022c029
Dharunkumar0510 <dharunkumarapd@gmail.com>
2026-02-26 13:05:24 +0530

    Add initial index.php for Dharun Homes website

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dharun Homes</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Welcome to Dharun Homes</h1>
+    <a href="add_property.php">Add Property</a> |
+    <a href="view_properties.php">View Properties</a>
+</body>
+</html>
